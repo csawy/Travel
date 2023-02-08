514ec106dc470cd619eb26340371e7d5e3b871cf
chenshuang <1031355547@qq.com>
2023-02-08 11:39:40 +0800

    city search

--- src/pages/City/City.vue
+++ src/pages/City/City.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <CityHeader></CityHeader>
+    <CitySearch></CitySearch>
   </div>
 </template>
 
 <script>
 import CityHeader from './component/Header.vue'
+import CitySearch from './component/Search.vue'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
